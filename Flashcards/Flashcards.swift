9a0b8d230d7ff8e066f5723e87d5a74d5f05cc26
iD Student <idstudent@stratton-twenty.mit.edu>
2017-08-02 13:25:31 -0400

    made it so that you can see answers in the list picker.

---
+++ Flashcards/Flashcards.swift
@@ -0,0 +1,52 @@
+//
+//  Flashcards.swift
+//  Flashcards
+//
+//  Created by iD Student on 8/1/17.
+//  Copyright © 2017 iD Tech. All rights reserved.
+//
+
+import Foundation
+
+class Flashcard {
+    var question : String
+    var options : [String]
+    var correctAnswer : String
+    
+    init( question : String, options : [String] ) {
+        
+        self.question = question
+        self.options = options
+        
+        self.correctAnswer = options[0]
+    }
+}
+
+class cardCollection {
+    public static var instance : cardCollection = cardCollection()
+    public private(set) var cards : [Flashcard]
+    public private(set) var currentIndex : Int
+    
+    public var currentCard : Flashcard {
+        get {return cards[currentIndex]}
+    }
+    
+    private init() {
+        cards = [ Flashcard(question: "Test Question", options: ["True", "False"]),
+                  Flashcard(question: "Who developed Swift?", options: ["Apple", "Google", "Microsoft", "Facebook"] ),
+                  Flashcard(question: "Who developed Windows?", options: ["Microsoft", "Google", "Apple", "Facebook"]),
+                  Flashcard(question: "What is the name of California's technology hub?", options: ["Silicon Valley", "Silicon Mountain", "Techworld", "China"])]
+        
+        currentIndex = 0;
+    }
+    public func nextQuestion() {
+        currentIndex += 1
+        if (currentIndex >= cards.count) {
+            currentIndex = 0
+        }
+    }
+    
+    public func checkAnswer(_selectedAnswer : Int) -> Bool {
+        return currentCard.options [_selectedAnswer] == currentCard.correctAnswer
+    }
+}
--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var questionTextView: UITextView!
+    
+    @IBOutlet weak var answerPickerView: UIPickerView!
+    
+    @IBOutlet weak var questionalLabel: UILabel!
+    
+        override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        answerPickerView.dataSource = self;
+        answerPickerView.delegate = self;
+        setupCardUI()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func setupCardUI() {
+        questionTextView.text = cardCollection.instance.currentCard.question
+        
+        questionalLabel.text = "Question \(cardCollection.instance.currentIndex + 1)/\(cardCollection.instance.cards.count)"
+    }
+    
+    // Pickerview Data Source
+    // returns the number of 'columns' to display.
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1;
+    }
+    
+    
+    // returns the # of rows in each component..
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return cardCollection.instance.currentCard.options.count
+    }
+    
+    // Pickerview delegates
+    // returns text of option at a given row.
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return cardCollection.instance.currentCard.options[row];
+    }
+    
 
 
 }
+
+
 
